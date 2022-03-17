1ba43a8893f34d6e113b580f7499c8b2d5167f0f
Mary Louise Hermosa <jayzces@gmail.com>
2022-03-17 16:29:22 +0800

    Modules 1-13

---
+++ components/HomeCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="float:left;margin:10px">
+      <img :src="home.images[0]" style="width:200px"/><br />
+      {{ home.title }}<br />
+      {{ home.location.address }}, {{ home.location.city }}, {{ home.location.state }}, {{ home.location.country }}<br />
+      {{ home.guests }} guests<br />
+      {{ home.pricePerNight }} / night<br />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      home: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
