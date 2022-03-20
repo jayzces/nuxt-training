6437c0b73f44e583226fd2c690dcac77819262fa
Mary Louise Hermosa <jayzces@gmail.com>
2022-03-20 12:56:25 +0800

    Modules 14-19

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,5 +16,8 @@ export default {
   },
   router: {
     prefetchLinks: false
-  }
+  },
+  plugins: [
+    '~/plugins/maps.client'
+  ]
 }
--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -13,6 +13,9 @@
     <img src="/images/marker.svg" width="20" height="20" /> {{ home.location.address }}, {{ home.location.city }}, {{ home.location.state }}, {{ home.location.country }}<br />
     <img src="/images/star.svg" width="20" height="20" /> {{ home.reviewValue }}<br />
     {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath <br />
+    {{ home.description }}
+
+    <div style="width:800px;height:800px" ref="map"></div>
   </div>
 </template>
 
@@ -32,6 +35,10 @@
     },
     created() {
       this.home = homes.find(home => home.objectID == this.$route.params.id)
+    },
+    // client-side only, in nuxt
+    mounted() {
+      this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
     }
   }
 </script>
